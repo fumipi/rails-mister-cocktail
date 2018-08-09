1d5c55fe137a9124ab250c85ca8fe44d9078b639
Fumiko Toyoda <toyodafumiko@gmail.com>
2018-08-09 20:58:23 +0900

    fixed delete link issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
-
   delete 'doses/:id', to: "doses#destroy", as: :dose
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
